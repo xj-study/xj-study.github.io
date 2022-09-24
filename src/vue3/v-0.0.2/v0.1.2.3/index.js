f062118a925396725f1084e20b8725193ac6d2c2
Jun.Xie8 <Jun.Xie8@lotuscars.com.cn>
2022-09-24 18:01:40 +0800

    feat: add v0032~v0035

--- src/vue3/v-0.0.2/v0.1.2.3/index.js
+++ src/vue3/v-0.0.2/v0.1.2.3/index.js
@@ -1,19 +1,32 @@
-// 新增修改
-const obj = new Proxy(
-  { text: 'hello world!' },
-  {
-    set(target, key, val) {
-      target[key] = val
+const obj = { text: 'hello world!' }
+
+// 遍历对象 obj 的所有属性，并对其做响应式处理
+const keys = Object.keys(obj)
+keys.forEach((prop) => {
+  defineReactive(obj, prop)
+})
+
+// 对指定对象 obj 的属性 prop 做响应处理
+function defineReactive(obj, prop) {
+  let value = obj[prop]
+  Object.defineProperty(obj, prop, {
+    get() {
+      return value
+    },
+    set(val) {
+      value = val
       effect()
     },
-  }
-)
+  })
+}
 
 effect()
+// ...省略相关代码
 setTimeout(() => {
-  obj.text = 'hello vue3'
+  // obj.text = 'hello vue3'
+  obj.desc = '你好吗？' // 新增
 }, 1000)
 
 function effect() {
-  document.body.innerText = obj.text
+  document.body.innerText = obj.text + obj.desc // 修改
 }
